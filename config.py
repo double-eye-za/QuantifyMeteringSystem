f7e2a6733c87079a6f8a7101f731aa18637782c3
Takudzwa <takudzwamaseva020@gmail.com>
2025-10-14 15:41:40 +0200

    feat: add initial database migration setup with Alembic configuration and schema definition

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "postgresql+psycopg2://postgres:postgres@localhost:5432/quantify",
+        "postgresql+psycopg2://localhost/quantify",
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
