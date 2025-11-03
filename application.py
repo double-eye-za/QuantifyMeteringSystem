8d4fc8fd1de87da9411945077f9f33debcca440c
martin-metalogix <martin@metalogix.solutions>
2025-11-03 15:08:56 +0200

    changed port

--- application.py
+++ application.py
@@ -148,4 +148,4 @@ def register_error_handlers(app: Flask):
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', debug=False, port=5000)
